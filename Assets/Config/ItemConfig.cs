75a84d47bd26813e7a0d106f03eec177ffcf82a9
stoarch <stormarchitextor@gmail.com>
2020-09-29 20:30:27 +0600

    Basic setup

---
+++ Assets/Config/ItemConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEditor;
+using UnityEngine;
+
+public enum ItemType
+{
+    Item1,
+    Item2, 
+    Item3
+}
+
+[CreateAssetMenu(fileName = "cfg", menuName = "ScriptableObjects/ItemConfig", order=1)]
+public class ItemConfig : ScriptableObject
+{
+    public int Weight;
+    public string Name;
+    public Guid UID;
+    public ItemType ItemType;
+    public string Id;
+    
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField]
+    private ItemConfig config;
+
+    private bool dragging = false;
+    private float distance;
+    private Camera mainCamera;
+    private Rigidbody body;
+
+    void Start()
+    {
+        mainCamera = Camera.main; //for performance 
+        if(mainCamera == null)
+        {
+            Debug.LogError("Unable to find main camera");
+            gameObject.SetActive(false);
+            return;
+        }
+
+        body = GetComponent<Rigidbody>();
+
+        if(body == null)
+        {
+            Debug.LogError("Unable to find component rigidbody");
+            gameObject.SetActive(false);
+            return;
+        }
+    }
+
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            var rayPoint = ray.GetPoint(distance);
+            transform.position = rayPoint;
+        }        
+    }
+
+    private void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, mainCamera.transform.position);
+        dragging = true;
+        body.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        dragging = false;
+        body.isKinematic = false;
+    }
+
+}
